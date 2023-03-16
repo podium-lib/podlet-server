419765a8de6b6993313d9a5f7832c19f3547728d
Richard Walker <digitalsadhu@gmail.com>
2023-03-16 14:26:08 +1300

    add types to assets plugin

--- plugins/assets.js
+++ plugins/assets.js
@@ -14,16 +14,29 @@ const isAbsoluteURL = (pathOrUrl) => {
 };
 
 /**
+ * @typedef {Object} AssetsOptions
+ * @property {string} [base="/static"]
+ * @property {string} [cwd=process.cwd()]
+ * @property {string} [dist="/dist"]
+ */
+
+/**
  * Mounts static file serving middleware into the app whenever the given base path is not absolute.
  * Typically, base would be absolute if you are serving static files from a CDN in which case static serving in the app is not needed.
  *
  * If the base path is relative, files in the dist folder are served at the base path
+ *
+ * @param {import("fastify").FastifyInstance} fastify
+ * @param {AssetsOptions} options
+ * @returns {Promise<void>}
  */
-export default fp(async function assets(fastify, { base = "/static", cwd = process.cwd(), dist = "/dist" }) {
+const assets = async function assets(fastify, { base = "/static", cwd = process.cwd(), dist = "/dist" }) {
   if (isAbsoluteURL(base)) return;
 
   await fastify.register(fastifyStatic, {
     root: join(cwd, dist),
     prefix: base,
   });
-});
+};
+
+export default fp(assets);
