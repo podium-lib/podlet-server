85b21a4df65b02b4e2fb35f6e39fbc5931284720
Richard Walker <digitalsadhu@gmail.com>
2023-04-21 15:11:00 +1200

    fix: replace dsd ponyfill with more comprehensive implementation

--- plugins/ssr.js
+++ plugins/ssr.js
@@ -1,17 +1,20 @@
-import { readFileSync } from "node:fs";
 import { html } from "lit";
 import { unsafeHTML } from "lit/directives/unsafe-html.js";
 import { render } from "@lit-labs/ssr";
 import fp from "fastify-plugin";
 
-const dsdPolyfill = readFileSync(new URL("../lib/dsd-polyfill.js", import.meta.url), { encoding: "utf8" });
-
-export default fp(async function ssrPlugin(fastify) {
-  fastify.decorate("ssr", function ssr(template) {
+export default fp(async function ssrPlugin(fastify, { appName = "", base = "/" }) {
+  fastify.decorate("ssr", function ssr(type, template) {
     // user provided markup, SSR'd
     const ssrMarkup = Array.from(render(html`${unsafeHTML(template)}`)).join("");
     // polyfill for browsers that don't support declarative shadow dom
-    const polyfillMarkup = `<script>${dsdPolyfill}</script>`;
+    const polyfillMarkup = `
+    <script type="module">
+    if (!HTMLTemplateElement.prototype.hasOwnProperty("shadowRoot")) {
+      const {hydrateShadowRoots} = await import("${base}/client/template-shadowroot.js");
+      hydrateShadowRoots(document.querySelector("${appName}-${type}"));
+    }
+    </script>`;
     return `${ssrMarkup}${polyfillMarkup}`;
   });
 });
