f91708354b4846c50c7323fe76aa58156776a34b
Richard Walker <digitalsadhu@gmail.com>
2023-06-13 11:19:03 +1200

    fix: replace unsafeHtml with unsafeStatic for tag names

--- plugins/ssr.js
+++ plugins/ssr.js
@@ -1,12 +1,10 @@
-import { html } from "lit";
-import { unsafeHTML } from "lit/directives/unsafe-html.js";
 import { render } from "@lit-labs/ssr";
 import fp from "fastify-plugin";
 
 export default fp(async function ssrPlugin(fastify, { appName = "", base = "/" }) {
   fastify.decorate("ssr", function ssr(type, template) {
     // user provided markup, SSR'd
-    const ssrMarkup = Array.from(render(html`${unsafeHTML(template)}`)).join("");
+    const ssrMarkup = Array.from(render(template)).join("");
     // polyfill for browsers that don't support declarative shadow dom
     const polyfillMarkup = `
     <script type="module">
