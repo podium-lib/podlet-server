64a4dfa75d17d00f3a35be798e48e2b102a6f071
Christoffer Jahren <christoffer.jahren@schibsted.com>
2023-06-27 09:22:26 +0200

    fix: small i18n improvements

--- plugins/locale.js
+++ plugins/locale.js
@@ -1,14 +1,10 @@
 import { join } from "node:path";
 import fs from "node:fs";
 import fp from "fastify-plugin";
-import PathResolver from "../lib/path.js";
 import chalk from "chalk";
 
-export default fp(async function locale(fastify, { cwd = process.cwd(), locale = "en", development }) {
-  const resolver = new PathResolver({ development, cwd });
-
-  const contentFile = await resolver.resolve("./content");
-  const compiledFileExtension = contentFile.typescript ? ".ts" : ".mjs";
+export default fp(async function locale(fastify, { cwd = process.cwd(), locale = "en" }) {
+  const compiledFileExtension = ".mjs";
 
   const localeFilePath = join(cwd, "locales", locale) + compiledFileExtension;
 
