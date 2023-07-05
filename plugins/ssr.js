10cfde2fe5a10a6f1f6f4a0f567a85a3b36c184d
Richard Walker <digitalsadhu@gmail.com>
2023-07-06 10:11:36 +1200

    fix: do not use development bundling urls in non development context (#98)
    * fix: do not use development bundling urls in non development context
    
    * test: fix tests

--- plugins/ssr.js
+++ plugins/ssr.js
@@ -2,9 +2,12 @@ import { render } from "@lit-labs/ssr";
 import fp from "fastify-plugin";
 import { joinURLPathSegments } from "../lib/utils.js";
 
-export default fp(async function ssrPlugin(fastify, { appName = "", prefix = "" }) {
+export default fp(async function ssrPlugin(fastify, { appName = "", base = "/", prefix = "", development = false }) {
   fastify.decorate("ssr", function ssr(type, template) {
-    const shadowRootPath = joinURLPathSegments(prefix, `/_/dynamic/modules/@webcomponents/template-shadowroot/template-shadowroot.js`);
+    let shadowRootPath = `${base}/client/template-shadowroot.js`;
+    if (development) {
+      shadowRootPath = joinURLPathSegments(prefix, `/_/dynamic/modules/@webcomponents/template-shadowroot/template-shadowroot.js`);
+    }
     // user provided markup, SSR'd
     const ssrMarkup = Array.from(render(template)).join("");
     // polyfill for browsers that don't support declarative shadow dom
