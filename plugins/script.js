813daa95ac03f1f158548f71c57b15dacdcd98e8
William Killerud <william.killerud@schibsted.com>
2023-10-27 09:37:49 +0200

    fix: don't render dev scripts in prod mode

--- plugins/script.js
+++ plugins/script.js
@@ -1,11 +1,18 @@
 import fp from 'fastify-plugin';
 
 export default fp(async (fastify, { development = false }) => {
-  fastify.decorate('script', (url, { dev = false, lazy = false } = {}) => {
-    if (!development && dev) return '';
-    if (lazy) {
-      return `<script type="module">addEventListener('load',()=>import('${url}'))</script>`;
-    }
-    return `<script type="module" src="${url}"></script>`;
-  });
+  fastify.decorate(
+    'script',
+    /**
+     * @param {string} url
+     * @param {{ dev?: boolean, lazy?: boolean }} options – dev: only in development, lazy: dynamic import on load event
+     */
+    (url, { dev = false, lazy = false } = {}) => {
+      if (!development && dev) return '';
+      if (lazy) {
+        return `<script type="module">addEventListener('load',()=>import('${url}'))</script>`;
+      }
+      return `<script type="module" src="${url}"></script>`;
+    },
+  );
 });
